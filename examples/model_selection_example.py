db92ff1099933a658e7a0e4b8cb455683cf4f9c1
Yue Zhao <zhaoy@cmu.edu>
2020-09-26 17:38:01 -0400

    update models

--- examples/model_selection_example.py
+++ examples/model_selection_example.py
@@ -20,17 +20,17 @@ sys.path.append(
 from pyod.utils.data import generate_data
 # from metaod.models.predict_metaod import select_model
 from metaod.models.gen_meta_features import generate_meta_features
+
 from metaod.models.utility import prepare_trained_model
+
 from metaod.models.core import MetaODClass
+from metaod.models.predict_metaod import select_model
 
 if __name__ == "__main__":
 
     contamination = 0.1  # percentage of outliers
     n_train = 200  # number of training points
     n_test = 100  # number of testing points
-    
-    # url = 'https://github.com/yzhao062/MetaOD/blob/master/saved_models/metaod_models.zip'
-    # urllib.request.urlretrieve(url, 'metaod_models.zip')
     
     # Generate sample data
     X_train, y_train, X_test, y_test = \
@@ -39,26 +39,29 @@ if __name__ == "__main__":
                       n_features=3,
                       contamination=contamination,
                       random_state=42)
+        
+    clf_setting = select_model(X_train, n_selection=10)
     
+    # # load and unzip models
     # prepare_trained_model()
 
-
-    meta_scalar = load(os.path.join("trained_models","meta_scalar.joblib"))
-    meta_X, _ = generate_meta_features(X_train)
-    meta_X = meta_scalar.transform(np.asarray(meta_X).reshape(1, -1))
+    # # load PCA scalar
+    # meta_scalar = load(os.path.join("trained_models","meta_scalar.joblib"))
+    # meta_X, _ = generate_meta_features(X_train)
+    # meta_X = meta_scalar.transform(np.asarray(meta_X).reshape(1, -1))
     
-    model_lists = list(load(os.path.join("trained_models","model_list.joblib")))
+    # model_lists = list(load(os.path.join("trained_models","model_list.joblib")))
     
-    # use all trained models for ensemble
-    trained_models = [
-        # 'test.pk'
-        "train_0.joblib", 
-        # "train_1.joblib", 
-        # "train_42.joblib"
-        ]
+    # # use all trained models for ensemble
+    # trained_models = [
+    #     # 'test.pk'
+    #     "train_0.joblib", 
+    #     # "train_1.joblib", 
+    #     # "train_42.joblib"
+    #     ]
     
-    for i, model in enumerate(trained_models):
-        clf = load(os.path.join("trained_models", model))
+    # for i, model in enumerate(trained_models):
+    #     clf = load(os.path.join("trained_models", model))
         
     # # get top 10 models
     # clf_setting = select_model(X_train, n_selection=10)
